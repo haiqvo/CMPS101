d368aa240d41d21f22769d56ccfb8d02b7c61051
Hai Vo <hai@hai-SVE15128CXS.(none)>
2013-01-20 23:01:04 -0800

    Found all the neighbors

--- GameOfLife/src/GridCell.java
+++ GameOfLife/src/GridCell.java
@@ -18,7 +18,7 @@ public class GridCell {
         
     //}
     ArrayList<Cells> aliveCells = new ArrayList();
-    ArrayList<Cells> CollectionsOfCells = new ArrayList();
+    ArrayList<Cells> collectionsOfCells = new ArrayList();
     public void createCells(int size1, int size2){
         grid = new Cells[size1][size2];
         for(int k = 0; k<size1; k++){
@@ -39,8 +39,28 @@ public class GridCell {
     }
     
     public void cycleCells(){
-        CollectionsOfCells.addAll(aliveCells);
+        for(int i = 0; i<aliveCells.size();i++){
+            CheckForRepeats(i, -1, -1);
+            CheckForRepeats(i, 0, -1);
+            CheckForRepeats(i, 1, -1);
+            CheckForRepeats(i, -1, 0);
+            CheckForRepeats(i, 1, 0);
+            CheckForRepeats(i, -1, 1);
+            CheckForRepeats(i, 0, 1);
+            CheckForRepeats(i, 1, 1);
+        }
         
+    }
+    public void CheckForRepeats(int i, int x, int y){
+        int xCor = aliveCells.get(i).x;
+        int yCor = aliveCells.get(i).y;
+        if(collectionsOfCells.indexOf(grid[xCor+x][yCor+y]) == -1){
+            if(xCor+x < aliveCells.size() || xCor+x > 0){
+                if(yCor+y < aliveCells.size() || yCor+y > 0){
+                    collectionsOfCells.add(grid[xCor+x][yCor+y]);
+                }
+            }
+        }
     }
     public void printTest(){
         for(int k = 0; k<grid.length; k++){
@@ -50,7 +70,8 @@ public class GridCell {
         }
     }
     public void printTest2(){
-        System.out.println(CollectionsOfCells.get(1).x);
-        System.out.println(grid[21][20].pointer);
+        System.out.println(collectionsOfCells);
+        System.out.println(grid[19][20]);
+        
     }
 }
